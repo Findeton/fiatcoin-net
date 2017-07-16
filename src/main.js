c650680fbc0d814eb94cea27bedd1ee796ce1035
Findeton <felrobelv@gmail.com>
2017-07-16 09:03:58 +0200

    refactor g analytics

--- src/main.js
+++ src/main.js
@@ -6,11 +6,17 @@ import 'bootstrap-vue/dist/bootstrap-vue.css';
 
 import Vue from 'vue';
 import BootstrapVue from 'bootstrap-vue';
+import VueAnalytics from 'vue-analytics';
 import App from './App';
 import router from './router';
 
 Vue.use(BootstrapVue);
 Vue.config.productionTip = false;
+
+Vue.use(VueAnalytics, {
+  id: 'UA-102416922-2',
+  router,
+});
 
 /* eslint-disable no-new */
 new Vue({
--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,8 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import VueAnalytics from 'vue-analytics';
 import Hello from '@/components/Hello';
 
 Vue.use(Router);
-
-Vue.use(VueAnalytics, {
-  id: 'UA-102416922-2',
-});
 
 export default new Router({
   mode: 'history',
